ee7e95ad532e776f9966db9c869c6e964f4796fb
League Student <league@iMac-37.attlocal.net>
2019-09-09 19:55:41 -0700

    Moles On Holes

--- src/_08_safe_cracker/SafeCracker.java
+++ src/_08_safe_cracker/SafeCracker.java
@@ -22,8 +22,9 @@ public class SafeCracker {
 		 * Your mission: use the tryCode method to crack the safe by trying all possible
 		 * combinations
 		 */
-		
-
+		for (int i = 0; i < 9999999; i++) {
+tryCode(i);
+		}		
 	}
 
 	static void tryCode(int guess) {
